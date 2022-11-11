c4395f5ecffc8b3a0c6c480f61a1d208b0aa68c4
thulagcc <thulagcc200109@fpt.edu.vn>
2022-11-11 17:21:52 +0700

    thu

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+
+// $server = "localhost";
+// $username ="root";
+// $password = "";
+// $db = "asm2_200312";
+
+// $conn = mysqli_connect($server,$username,$password,$db);
+// if($conn->connect_error){
+//     die("Failed ".$conn->connect_error);
+// }
+
+$conn = pg_connect("postgres://kpxccmyoqzkujz:cdc818481992e58d6b88fa62b490c7c92257f6fde993c7f7670362fb1b51fa21@ec2-3-93-206-109.compute-1.amazonaws.com:5432/d2uuuugm90ocgr");
+
+// $conn = pg_connect("host=localhost port=5432 dbname=asm2_200312 user=postgres password=Pass?*n@1");
+
+if (!$conn) {
+    die("Connection failed");
+}
+
+if ($conn){
+    echo "connected";
+}
+?>
---
+++ manager.php
@@ -0,0 +1,317 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Electro Store Ecommerce Category Bootstrap Responsive Web Template | Contact Us :: w3layouts</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Electro Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"
+	/>
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- Bootstrap css -->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- Main css -->
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- pop-up-box -->
+	<link href="css/menu.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- menu style -->
+	<!-- //Custom-Files -->
+
+	<!-- web fonts -->
+	<link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<!-- //web fonts -->
+
+</head>
+
+<body>
+	<!-- top-header -->
+	<div class="agile-main-top">
+		<div class="container-fluid">
+			<div class="row main-top-w3l py-2">
+				
+				<div class="col-lg-8 header-right mt-lg-0 mt-2">
+					<!-- header lists -->
+					<ul>
+						<li class="text-center border-right text-white">
+							<i class="fas fa-phone mr-2"></i> 001 234 5678
+						</li>
+						<li class="text-center border-right text-white">
+							<a href="#" data-toggle="modal" data-target="#exampleModal" class="text-white">
+								<i class="fas fa-sign-in-alt mr-2"></i> Log In </a>
+						</li>
+						<li class="text-center text-white">
+							<a href="#" data-toggle="modal" data-target="#exampleModal2" class="text-white">
+								<i class="fas fa-sign-out-alt mr-2"></i>Register </a>
+						</li>
+					</ul>
+					<!-- //header lists -->
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Button trigger modal(select-location) -->
+	<div id="small-dialog1" class="mfp-hide">
+		
+	</div>
+	<!-- //shop locator (popup) -->
+
+	<!-- modals -->
+	<!-- log in -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title text-center">Log In</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form action="#" method="post">
+						<div class="form-group">
+							<label class="col-form-label">Username</label>
+							<input type="text" class="form-control" placeholder=" " name="Name" required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Password</label>
+							<input type="password" class="form-control" placeholder=" " name="Password" required="">
+						</div>
+						<div class="right-w3l">
+							<input type="submit" class="form-control" value="Log in">
+						</div>
+						<div class="sub-w3l">
+							<div class="custom-control custom-checkbox mr-sm-2">
+								<input type="checkbox" class="custom-control-input" id="customControlAutosizing">
+								<label class="custom-control-label" for="customControlAutosizing">Remember me?</label>
+							</div>
+						</div>
+						<p class="text-center dont-do mt-3">Don't have an account?
+							<a href="#" data-toggle="modal" data-target="#exampleModal2">
+								Register Now</a>
+						</p>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- register -->
+	<div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title">Register</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form action="#" method="post">
+						<div class="form-group">
+							<label class="col-form-label">Your Name</label>
+							<input type="text" class="form-control" placeholder=" " name="Name" required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Email</label>
+							<input type="email" class="form-control" placeholder=" " name="Email" required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Password</label>
+							<input type="password" class="form-control" placeholder=" " name="Password" id="password1" required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Confirm Password</label>
+							<input type="password" class="form-control" placeholder=" " name="Confirm Password" id="password2" required="">
+						</div>
+						<div class="right-w3l">
+							<input type="submit" class="form-control" value="Register">
+						</div>
+						<div class="sub-w3l">
+							<div class="custom-control custom-checkbox mr-sm-2">
+								<input type="checkbox" class="custom-control-input" id="customControlAutosizing2">
+								<label class="custom-control-label" for="customControlAutosizing2">I Accept to the Terms & Conditions</label>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //modal -->
+	<!-- //top-header -->
+
+	<!-- header-bottom-->
+	<div class="header-bot">
+		<div class="container">
+			<div class="row header-bot_inner_wthreeinfo_header_mid">
+				<!-- logo -->
+				<div class="col-md-3 logo_agile">
+					<h1 class="text-center">
+						<a href="index.html" class="font-weight-bold font-italic">
+							<img src="images/logo2.png" alt=" " class="img-fluid">Electro Store
+						</a>
+					</h1>
+				</div>
+				<!-- //logo -->
+				<!-- header-bot -->
+				<div class="col-md-9 header mt-4 mb-md-0 mb-4">
+					<div class="row">
+						<!-- search -->
+						<div class="col-10 agileits_search">
+							<form class="form-inline" action="#" method="post">
+								<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" required>
+								<button class="btn my-2 my-sm-0" type="submit">Search</button>
+							</form>
+						</div>
+						<!-- //search -->
+
+					
+						<!-- //cart details -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- shop locator (popup) -->
+	<!-- //header-bottom -->
+	<!-- navigation -->
+	<div class="navbar-inner">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+				    aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto text-center">
+						<li class="nav-item mr-lg-4 mb-lg-0 mb-4">
+							<a class="nav-link" href="index.php">Home
+							</a>
+						</li>
+						
+						<li class="nav-item mr-lg-4 mb-lg-0 mb-4">
+							<a class="nav-link" href="product.php">Product</a>
+						</li>
+						<li class="nav-item mr-lg-4 mb-lg-0 mb-4">
+							<a class="nav-link" href="manager.php">Manager</a>
+						</li>
+						
+						<li class="nav-item active">
+							<a class="nav-link" href="contact.html">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- //navigation -->
+
+	<!-- banner-2 -->
+	<div class="page-head_agile_info_w3l">
+
+	</div>
+	<!-- //banner-2 -->
+	<!-- page -->
+	<div class="services-breadcrumb">
+		<div class="agile_inner_breadcrumb">
+			<div class="container">
+				<ul class="w3_short">
+					<li>
+						<a href="index.html">Home</a>
+						<i>|</i>
+					</li>
+					<li>Contact Us</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- //page -->
+	<div id="main">
+        <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+       
+                <div style="text-align: center;" className="page-heading pb-2 mt-4 mb-2 ">
+                    <h1>Manager</h1> 
+                    <a href="insert.php"><button type="button" class="btn btn-outline-primary">Insert</button></a>
+                </div>
+                <div class="page-content">
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
+
+                
+        <?php
+            $sql1 = "SELECT * from product";
+            $rel1 = pg_query($conn,$sql1);
+        ?>
+               
+                </div>
+                <div class="container mb-3">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">DetailDesc</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Catogery</th>
+                            <th style="text-align: center;" scope="col" colspan="2">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                while($row = pg_fetch_assoc($rel1) ) {
+                            ?>
+                            <tr id="table">
+                                <td id="table"><?=$row['p_id']?></td>
+                                <td id="table"><?=$row['p_name']?></td>
+                                <td id="table"><?=$row['p_price']?></td>
+                                <td style="text-align: left;"  id="table"><?=$row['DetailDesc']?></td>
+                                <td id="table"><?=$row['p_quantity']?></td>
+                                <td id="table"><?=$row['cat_id']?></td>
+                                <td>
+                                    <a href="update.php?id=<?=$row['p_id']?>" class="btn btn-warning rounded-pill">Update</a> 
+                                </td>
+                                <td>
+                                    <a href="delete.php?id=<?=$row['p_id']?>" class="btn btn-warning rounded-pill">Delete</a> 
+                                </td>
+                            </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                </div>
+                <br>
+                <br>
+    </div>
+	
+</body>
+
+</html>
